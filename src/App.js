8f21ff92e08167f1595a1953ef0421653a9b9ed6
SylvainBR <3095440-Sylvainn7@users.noreply.gitlab.com>
2021-03-26 16:30:28 +0100

    make-a-winner

--- src/App.js
+++ src/App.js
@@ -24,13 +24,22 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     }
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    console.log(`WINNER?`);
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
+    console.log(`HANDLECLICK\n ${squares}`);
   }
   
   renderSquare(i) {
@@ -43,8 +52,14 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
-
+    console.log(`RENDER\n ${this.state.squares}`);
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
     return (
       <div>
         <div className="status">{status}</div>
@@ -78,3 +93,23 @@ export default class Game extends React.Component {
   }
 }
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    console.log(`CALCULATE\n ${squares[a]}\t${squares[b]}\t${squares[c]}`);
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
